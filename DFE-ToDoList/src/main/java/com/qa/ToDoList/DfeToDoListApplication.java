246b9f0df95f69722ef3394a9b8705f751fdb9b6
JackMcAulay <jackmcaulay0711@gmail>
2021-11-22 11:19:40 +0000

    Project Initialised

---
+++ DFE-ToDoList/src/main/java/com/qa/ToDoList/DfeToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.qa.ToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfeToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfeToDoListApplication.class, args);
+	}
+
+}
