c2ec2a97154e601ff3296c4128a6c7c834eb58b6
JackMcAulay <47449092+JackMcAulay@users.noreply.github.com>
2021-11-24 15:34:21 +0000

    Update UserServiceTests.java

--- DFE-ToDoList/src/test/java/com/qa/ToDoList/Services/UserServiceTests.java
+++ DFE-ToDoList/src/test/java/com/qa/ToDoList/Services/UserServiceTests.java
@@ -1,6 +1,11 @@
 package com.qa.ToDoList.Services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,24 +18,82 @@ import com.qa.ToDoList.Entities.UserRepository;
 
 @SpringBootTest
 public class UserServiceTests {
-
+	private static User testUser;
+	private static UserDTO testUserDTO;
+	private static List<User> testUserList;
+	private static List<UserDTO> testUserListDTO;
+	
 	@Autowired
 	private UserService service;
 	
 	@MockBean
 	private UserRepository repo;
 	
-	@Test
-	void createTest() {
-		final User testUser = new User(1L, "John", "Smith", "jsmith@gmail.com");
-		final User testUserSaved = new User(1L, "John", "Smith", "jsmith@gmail.com");
-		final UserDTO testUserDTO = new UserDTO();
+	@BeforeAll
+	public static void before() {
+		testUser = new User(1L, "John", "Smith", "jsmith@gmail.com");
+		testUserDTO = new UserDTO();
 		testUserDTO.setID(1L);
 		testUserDTO.setFullName("John Smith");
 		testUserDTO.setEmail("jsmith@gmail.com");
 		
-		Mockito.when(this.repo.save(testUser)).thenReturn(testUserSaved);
+		testUserList = new ArrayList<User>();
+		testUserList.add(testUser);
+		
+		testUserListDTO = new ArrayList<UserDTO>();
+		testUserListDTO.add(testUserDTO);
+	}
+	
+	@Test
+	void createTest() {
+		Mockito.when(this.repo.save(testUser)).thenReturn(testUser);
 		Assertions.assertThat(this.service.create(testUser)).isEqualTo(testUserDTO);
 		Mockito.verify(this.repo, Mockito.times(1)).save(testUser);
+	}
+	
+	@Test
+	void createMultipleTest() {
+		Mockito.when(this.repo.save(testUser)).thenReturn(testUser);
+		Assertions.assertThat(this.service.createMultiple(testUserList)).isEqualTo(testUserListDTO);
+		Mockito.verify(this.repo, Mockito.times(1)).save(testUser);
+	}
+	
+	@Test
+    public void readAllTest() {
+		testUserList.add(testUser);
+		testUserListDTO.add(testUserDTO);
+
+		Mockito.when(this.repo.findAll()).thenReturn(testUserList);
+		Assertions.assertThat(this.service.readAll()).isEqualTo(testUserListDTO);
+		Mockito.verify(this.repo, Mockito.times(1)).findAll();
+    }
+	
+	@Test
+    public void readByIdTest() {
+		final Optional<User> testUser = Optional.ofNullable(new User(1L, "John", "Smith", "jsmith@gmail.com"));
+		
+		Mockito.when(this.repo.findById(1L)).thenReturn(testUser);
+		Assertions.assertThat(this.service.readById(1L)).isEqualTo(testUserDTO);
+		Mockito.verify(this.repo, Mockito.times(1)).findById(1L);
+    }
+	
+	@Test
+	public void updateTest() {
+		final Optional<User> testUser = Optional.ofNullable(new User(1L, "John", "Smith", "jsmith@gmail.com"));
+		User updatedTestUser = new User(1L, "Jon", "Smith", "jsmith@yahoo.com");
+		UserDTO updatedTestUserDTO = new UserDTO();
+		updatedTestUserDTO.setID(1L);
+		updatedTestUserDTO.setFullName("Jon Smith");
+		updatedTestUserDTO.setEmail("jsmith@yahoo.com");
+		
+		Mockito.when(this.repo.findById(1L)).thenReturn(testUser);
+		Assertions.assertThat(this.service.update(1L, updatedTestUser)).isEqualTo(updatedTestUserDTO);
+		Mockito.verify(this.repo, Mockito.times(1)).findById(1L);
+	}
+	
+	@Test
+	public void deleteTest() {
+		this.service.delete(1L);
+		Mockito.verify(this.repo, Mockito.times(1)).deleteById(1L);
 	}
 }
